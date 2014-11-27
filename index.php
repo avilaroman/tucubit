e313fab17d78ef1e0ddea05d0fb054598b847644
avilaroman <avilaroman@gmail.com>
2014-11-26 21:56:50 -0300

    metatags

--- index.php
+++ index.php
@@ -6,27 +6,17 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="">
-    <meta name="author" content="">
-
+    <meta name="author" content="avilaroman">
+    <meta name="Description" CONTENT="Recarga la Tarjeta Ciudadana con Bitcoins!">
+    <meta name="robots" content="noindex,nofollow">
+    <meta name="keywords" content="tucubit,tucubit.com,avilaroman,bitcoins,bitcoin,tucuman,monteros,tucumán,san miguel de tucuman,tarjeta ciudadana,ciudadana,ciudadana bitcoin,ciudadana bitcoins,btc,xtc, recarga de tarjeta ciudadana,tarjetas ciudadanas">
     <title>TucuBit.com - Bitcoins en Pesos</title>
 
-    <!-- Bootstrap Core CSS -->
     <link href="css/bootstrap.min.css" rel="stylesheet">
 
-    <!-- Custom CSS -->
     <link href="css/stylish-portfolio.css" rel="stylesheet">
-
-    <!-- Custom Fonts -->
     <link href="font-awesome-4.1.0/css/font-awesome.min.css" rel="stylesheet" type="text/css">
     <link href="http://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,700,300italic,400italic,700italic" rel="stylesheet" type="text/css">
-
-    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
-    <![endif]-->
 
 </head>
 
@@ -82,8 +72,6 @@
         <!-- /.container -->
     </section>
 
-    <!-- Services -->
-    <!-- The circle icons use Font Awesome's stacked icon classes. For more information, visit http://fontawesome.io/examples/ -->
     <section id="services" class="services bg-primary">
         <div class="container">
             <div class="row text-center">
